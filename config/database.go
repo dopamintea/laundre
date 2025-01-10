5fd0287d380a6bf80239f6f1bccad4d173653b8a
Ridzkynanda <ridzkynanda.aditya@gmail.com>
2025-01-10 16:22:37 +0700

    [CLEAN]

--- config/database.go
+++ config/database.go
@@ -8,8 +8,8 @@ import (
 )
 
 func ConnectDatabase() (*gorm.DB, error) {
-	// Replace with your MySQL credentials
-	dsn := "user:P@ssword123@tcp(127.0.0.1:3306)/laundre?charset=utf8mb4&parseTime=True&loc=Local"
+
+	dsn := "user:pass@tcp(127.0.0.1:3306)/database?charset=utf8mb4&parseTime=True&loc=Local"
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
